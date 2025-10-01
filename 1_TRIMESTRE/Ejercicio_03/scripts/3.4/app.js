2c39c1a457fdcbc9abda290602fa646471995566
EneasMenendez <eneasdmp36@educastur.es>
2025-10-01 09:54:27 +0200

    Ejercicio 3.4 terminado

--- 1_TRIMESTRE/Ejercicio_03/scripts/3.4/app.js
+++ 1_TRIMESTRE/Ejercicio_03/scripts/3.4/app.js
@@ -1,19 +1,19 @@
 import { agregarLibro, obtenerLibros, buscarLibro, eliminarLibro } from './biblioteca.js';
 
-console.log("Colección inicial de libros:");
+console.log("Colección inicial:");
 console.log(obtenerLibros());
 
 const libroNuevo = { id: 11, titulo: "Rayuela", autor: "Julio Cortázar", paginas: 468 };
 agregarLibro(libroNuevo);
 
-console.log("Colección después de añadir un libro:");
+console.log("Coleccion +1 libro:");
 console.log(obtenerLibros());
 
 const libroBuscado = buscarLibro(3);
-console.log("Libro buscado con id 3:");
+console.log("Libro 3:");
 console.log(libroBuscado);
 
 eliminarLibro(2);
 
-console.log("Colección después de eliminar el libro con id 2:");
+console.log("Coleccion - libro 2:");
 console.log(obtenerLibros());
