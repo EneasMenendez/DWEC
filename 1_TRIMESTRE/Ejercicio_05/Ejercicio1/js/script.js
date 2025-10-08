4da3c7f363858292cd551c3e6764c94595c60d01
EneasMenendez <eneasdmp36@educastur.es>
2025-10-08 10:29:35 +0200

    Ejercicio 5

---
+++ 1_TRIMESTRE/Ejercicio_05/Ejercicio1/js/script.js
@@ -0,0 +1,29 @@
+function cambiarImagenPrincipal(indice) {
+  const miniaturas = document.querySelectorAll('.miniatura');
+  const imagenPrincipal = document.getElementById('imagen-principal');
+
+  if (miniaturas[indice]) {
+    const nuevaSrc = miniaturas[indice].getAttribute('src');
+    imagenPrincipal.setAttribute('src', nuevaSrc);
+    resaltarMiniatura(indice);
+  }
+}
+
+function resaltarMiniatura(indice) {
+  const miniaturas = document.querySelectorAll('.miniatura');
+
+  miniaturas.forEach((miniatura, i) => {
+    if (i === indice) {
+      miniatura.classList.add('activa');
+    } else {
+      miniatura.classList.remove('activa');
+    }
+  });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const miniaturas = document.querySelectorAll('.miniatura');
+  miniaturas.forEach((miniatura, indice) => {
+    miniatura.addEventListener('click', () => cambiarImagenPrincipal(indice));
+  });
+});
