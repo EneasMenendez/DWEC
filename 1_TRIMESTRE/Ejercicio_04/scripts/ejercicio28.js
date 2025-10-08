f194daaf3f489cb86b829f62a550ac543b9b7e6f
EneasMenendez <eneasdmp36@educastur.es>
2025-10-08 08:40:45 +0200

    Cambios

--- 1_TRIMESTRE/Ejercicio_04/scripts/ejercicio28.js
+++ 1_TRIMESTRE/Ejercicio_04/scripts/ejercicio28.js
@@ -1,1 +1,2 @@
-﻿const categorias=Array.from(document.querySelectorAll(".card .categoria")).map(c=>c.textContent); console.log(categorias);
+﻿const categorias=Array.from(document.querySelectorAll(".card .categoria")).map(c=>c.textContent); 
+console.log(categorias);
