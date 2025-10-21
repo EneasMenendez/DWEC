97c98ccaed4712a17218f5d92c3011d09ac369e2
EneasMenendez <eneasdmp36@educastur.es>
2025-10-21 10:52:57 +0200

    Ejercicio 7

---
+++ 1_TRIMESTRE/Ejercicio_07/scripts/main.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('%cDocumento listo.', 'color: green; font-size: 16px; font-weight: bold;');
+    console.log('%cEscribe las soluciones en main.js', 'color: red; font-size: 18px; font-weight: bold;');
+
+
+    // --- Solución Ejercicio 1 y 4 ---
+
+
+    // --- Solución Ejercicio 2 ---
+
+
+    // --- Solución Ejercicio 3 ---
+
+
+    // --- Solución Ejercicio 5 ---
+
+});
